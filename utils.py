fc9dfab81e11f71d92b9ce0976b5c3ba601e907b
goulustis <goulusti@student.ubc.ca>
2024-08-21 23:04:16 -0700

    mov colap manager to scene manager

--- utils.py
+++ utils.py
@@ -5,8 +5,6 @@ import os
 import bisect
 import h5py
 import json
-import cv2
-import os.path as osp
 
 import colmap_read_model as read_model
 
@@ -92,129 +90,6 @@ class EventBuffer:
 
     def valid_time(self, st_t):
         return st_t < self.t_f[-1]
-
-class ColmapCamera:
-
-    def __init__(self, cam_f):
-        self.col_cam = read_model.read_cameras_binary(cam_f)[1]
-        self.w = self.col_cam.width
-        self.h = self.col_cam.height
-        
-        self.fx, self.fy, self.cx, self.cy = self.col_cam.params[:4]
-        self.intrxs = np.array([[self.fx,     0 , self.cx],
-                                [0,      self.fy, self.cy],
-                                [0,            0,       1]])
-        self.k1, self.k2, self.p1, self.p2 = self.col_cam.params[4:]
-
-    def get_dist_coeffs(self):
-        return np.array([self.k1, self.k2, self.p1, self.p2])
-
-class ColmapSceneManager:
-    """
-    retrieves corresponding 3d points and plot them in 3d
-    """
-    def __init__(self, colmap_dir, sample_method="reliable"):
-        """
-        colmap_dir = xxx_recons/
-                         /images
-                         /sparse
-        """
-        self.colmap_dir = colmap_dir
-        self.img_dir = osp.join(colmap_dir, "images")
-
-        self.images = read_model.read_images_binary(osp.join(self.colmap_dir, "sparse/0/images.bin"))
-        self.camera = ColmapCamera(osp.join(self.colmap_dir, "sparse/0", "cameras.bin"))
-        self.img_fs = self.get_all_registered_img_fs()
-        self.chosen_points = None
-        self.pnts_2d=None
-
-        self.max_images_id = max(list(self.images.keys()))
-
-    def set_sample_method(self, method):
-        self.sample_method = method
-        self.sample_pnt_fnc = self.sample_pnt_fnc_dic[self.sample_method]
-    
-    @property
-    def image_ids(self):
-        return sorted(self.images.keys())
-    
-    def load_image(self, img_idx):
-        img_cam = self.images[img_idx]
-        img_name = img_cam.name # get img_name from img_cam
-        img_f = osp.join(self.img_dir, img_name)
-        return cv2.imread(img_f)
-
-
-
-    def __len__(self):
-        return len(self.images)
-
-
-    def get_extrnxs(self, img_idx=None, img_obj=None):
-        """
-        img_idx: colmap index
-        """
-        if img_idx < 0:
-            img_idx = self.max_images_id + img_idx
-            
-        if img_idx is not None:
-            img_obj = self.images[img_idx]
-        
-        R = img_obj.qvec2rotmat()
-        t = img_obj.tvec[..., None]
-        mtx = np.concatenate([R,t], axis=-1)
-        dummy = np.zeros((1,4))
-        dummy[0,-1] = 1
-        mtx = np.concatenate([mtx, dummy], axis=0)
-        return mtx
-
-    def get_all_extrnxs(self):
-        keys = sorted(list(self.images.keys()))
-        return [self.get_extrnxs(e) for e in keys]
-
-    def get_intrnxs(self):
-        return self.camera.intrxs, self.camera.get_dist_coeffs()
-
-    def get_img(self, img_idx):
-        """
-        img_idx = colmap_img_idx
-        """
-        if img_idx > 0: 
-            print("colmap img is 1 indexed! Adding 1 to idx")
-            img_idx += 1
-        return cv2.imread(self.get_img_f(img_idx))
-
-    def get_all_imgs(self, read_img_fn=cv2.imread):
-        return parallel_map(lambda f : read_img_fn(f), self.img_fs)
-
-    def get_img_f(self, img_idx):
-        if img_idx == 0:
-            print("image_idx needs to be >=1, adding 1 as fix")
-            img_idx += 1
-        if img_idx < 0:
-            img_idx = self.max_images_id + img_idx
-        return osp.join(self.img_dir, self.images[img_idx].name)
-
-    
-    def get_all_registered_img_fs(self):
-        names = sorted([v.name for k, v in self.images.items()])
-        return [osp.join(self.img_dir, name) for name in names]
-
-    def get_img_id(self, img_idx):
-        img_f = self.get_img_f(img_idx)
-        return osp.basename(img_f).split(".")[0]
-    
-
-    def get_found_cond(self, n_size):
-        """
-        return condition showing which image was registered
-        """
-        keys = np.array(sorted(list(int(k) for k in self.images.keys()))) - 1 # subtract 1 since colmap idx starts at 1
-        keys = keys[keys < n_size].astype(np.int32)
-        cond = np.zeros(n_size, dtype=bool)
-        cond[keys] = True
-        return cond
-
 
 def read_rel_cam(cam_rel_path):
     with open(cam_rel_path, "r") as f:
@@ -359,4 +234,13 @@ def parallel_map(f, iterable, max_threads=None, show_pbar=False, desc="", **kwar
         else:
             results = executor.map(f, iterable, **kwargs)
         return list(results)
-  
+
+
+def load_poses_bounds(path):
+    poses_arr = np.load(path)
+    poses = poses_arr[:, :-2].reshape([-1, 3, 5]).transpose([1,2,0])
+    bds = poses_arr[:, -2:].transpose([1,0])
+    hwf = poses[:, 4:, :].squeeze()
+
+    # shapes = (3,5,n), (2,n), (3,n)
+    return poses, bds, hwf
